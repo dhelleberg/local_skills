c0f0ba3743c32278a3e2c3477dc613416bdea1f0
Dominik Helleberg <dominik.helleberg@inovex.de>
2017-02-27 21:10:08 +0100

    added simple basic auth

--- local-server/index.js
+++ local-server/index.js
@@ -3,6 +3,7 @@
 const mqtt = require('mqtt');
 const http = require('http');
 const url = require('url');
+const auth = require('basic-auth')
 
 var mqttValues = new Map();
 
@@ -27,17 +28,30 @@ client.on('message', function (topic, message) {
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "application/json"});
-  var queryData = url.parse(request.url, true).query;
-  console.log(queryData);
-  if(queryData.datapoint && mqttValues.has(queryData.datapoint)) {
-    console.log(mqttValues.get(queryData.datapoint)+" "+queryData.datapoint);
-    var result = {};
-    result[queryData.datapoint] = mqttValues.get(queryData.datapoint);
-    response.end(JSON.stringify(result));
+  var user = process.argv[2];
+  var pwd = process.argv[3];
+  var credentials = auth(request);
+
+  if (!credentials || credentials.name !== user || credentials.pass !== pwd) {
+    response.statusCode = 401;
+    response.setHeader('WWW-Authenticate', 'Basic realm="example"');
+    response.end('Access denied');
+  } else {
+    response.writeHead(200, {"Content-Type": "application/json"});
+    var queryData = url.parse(request.url, true).query;
+    console.log(queryData);
+    if(queryData.datapoint && mqttValues.has(queryData.datapoint)) {
+        console.log(mqttValues.get(queryData.datapoint)+" "+queryData.datapoint);
+        var result = {};
+        result[queryData.datapoint] = mqttValues.get(queryData.datapoint);
+        response.end(JSON.stringify(result));
+    }
+    response.end("{}");
   }
-  response.end("{}");
 });
+
+if(!process.argv || process.argv.length < 4)
+  process.exit(-1);
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
